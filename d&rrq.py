dad6464179f12d14a50c48958ac5c8b434cd8648
ASAGAMING123 <113528724+ASAGAMING123@users.noreply.github.com>
2023-09-10 18:55:57 +0530

    yy

---
+++ d&rrq.py
@@ -0,0 +1,8 @@
+list1 = ['Red','Green','Blue','Yellow','Black','maroon']
+for item in list1:
+    print(item)
+dict1 = {'mango':12,'orange':45,'guava':27,'banana':10}
+for key in dict1:
+    print(key,':',dict1[key])
+for key,value in dict1.items():
+    print(key,':',value)
