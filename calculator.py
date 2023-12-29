e52ce8592f3b4f8cde0173bd35a46a51c328b072
ASAGAMING123 <113528724+ASAGAMING123@users.noreply.github.com>
2023-12-29 15:51:48 +0530

    bbhk

--- calculator.py
+++ calculator.py
@@ -1,26 +1,30 @@
-num_1 = complex(input("Enter a number:"))
-operator = str(input("Enter the operator:"))
-num_2 = complex(input("Enter a number:"))
-if operator == "+":
- num_3 = num_1 + num_2
- print(f"The result is {num_3}.")
-elif operator == "-":
- num_3 = num_1 - num_2
- print(f"The result is {num_3}.")
-elif operator == "*":
- num_3 = num_1 * num_2
- print(f"The result is {num_3}.")
-elif operator == "/":
- num_3 = num_1 / num_2
- print(f"The result is {num_3}.")
-elif operator == "^":
- num_3 = num_1 ** num_2
- print(f"The result is {num_3}.")
-elif operator == "|":
- num_3 = num_1 ** (1/num_2)
- print(f"The result is {num_3}.")
-elif operator == "%":
- num_3 = (num_1/100) * num_2
- print(f"The result is {num_3}.")
-else:
- print("Invalid operator.")
+while True:
+ num_1 = float(input("Enter a number:"))
+ operator = str(input("Enter the operator:"))
+ num_2 = float(input("Enter a number:"))
+ if operator == "+":
+  num_3 = num_1 + num_2
+  print(num_1,operator,num_2,"=",num_3,"\n")
+ elif operator == "-":
+  num_3 = num_1 - num_2
+  print(num_1,operator,num_2,"=",num_3,"\n")
+ elif operator == "*" or operator == "x":
+  num_3 = num_1 * num_2
+  print(num_1,operator,num_2,"=",num_3,"\n")
+ elif operator == "/":
+  if num_2==0:
+    print(num_1,operator,num_2,"= Infinity","\n")
+  else:
+    num_3 = num_1 / num_2
+    print(num_1,operator,num_2,"=",num_3,"\n")
+ elif operator == "^":
+  num_3 = num_1 ** num_2
+  print(num_1,operator,num_2,"=",num_3,"\n")
+ elif operator == "|":
+  num_3 = num_1 ** (1/num_2)
+  print(num_1,operator,num_2,"=",num_3,"\n")
+ elif operator == "%":
+  num_3 = (num_1/100) * num_2
+  print(num_1,operator,num_2,"=",num_3,"\n")
+ else:
+  print("Invalid operation!","\n")
